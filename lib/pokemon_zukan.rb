ef6e5e2bab1e6381fb35ac1d6d28451b930f9f3f
keitarou.oonishi <keitarou.oonishi@gmail.com>
2013-10-13 21:59:10 +0900

    add method find_all

--- lib/pokemon_zukan.rb
+++ lib/pokemon_zukan.rb
@@ -13,12 +13,20 @@ class PokemonZukan
   end
 
   def self.find(no="", series="xy")
-
+    no = format("%03d", no)
     file = File.open("#{DATA_DIR}/#{series}/#{no}.json")
     json = JSON.parser.new(file.read())
     file.close
 
     hash = json.parse()
     return PokemonZukan.new(hash)
+  end
+
+  def self.find_all(nos=[], series="xy")
+    retData = []
+    nos.each do |no|
+      retData.push self.find(no, series)
+    end
+    return retData
   end
 end
