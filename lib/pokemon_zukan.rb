b4d5365c10ea4e181c7d0f2c9f76cc818efde4f9
keitarou <keitarou.oonishi@gmail.com>
2014-03-27 01:22:24 +0900

    リファクタリング

--- lib/pokemon_zukan.rb
+++ lib/pokemon_zukan.rb
@@ -21,8 +21,9 @@ end
 
 class PokemonZukan
 
-  DATA_DIR = File.dirname(__FILE__) + '/../data'
-  LANGUAGE = 'ja'
+  @@series   = 'xy'
+  @@language = 'ja'
+  @@data_dir = File.dirname(__FILE__) + '/../data'
 
   attr_reader :no, :name, :type, :classification, :tribeValue, :height, :weight
 
@@ -36,9 +37,13 @@ class PokemonZukan
     @weight         = data["weight"]
   end
 
-  def self.find(no="", series="xy")
+  def self.file_dir
+    "#{@@data_dir}/#{@@series}/#{@@language}/"
+  end
+
+  def self.find(no="")
     no = format("%03d", no.to_i)
-    file = File.open("#{DATA_DIR}/#{series}/#{LANGUAGE}/#{no}.json")
+    file = File.open("#{PokemonZukan.file_dir}/#{no}.json")
     json = JSON.parser.new(file.read())
     file.close
 
@@ -46,41 +51,41 @@ class PokemonZukan
     PokemonZukan.new(hash)
   end
 
-  def self.find_all(nos=[], series="xy")
-    nos.map{|no| PokemonZukan::find(no, series)}
+  def self.find_all(nos=[])
+    nos.map{|no| PokemonZukan::find(no)}
   end
 
-  def self.find_by_name(name="", series="xy")
-    file = File.open("#{DATA_DIR}/#{series}/#{LANGUAGE}/name_table.json")
+  def self.find_by_name(name="")
+    file = File.open("#{PokemonZukan.file_dir}/name_table.json")
     json = JSON.parser.new(file.read())
     file.close
 
     hash = json.parse()
-    PokemonZukan::find(hash[name], series)
+    PokemonZukan::find(hash[name])
   end
 
-  def self.find_all_by_name(names=[], series="xy")
-    names.map{|name| PokemonZukan::find_by_name(name, series)}
+  def self.find_all_by_name(names=[])
+    names.map{|name| PokemonZukan::find_by_name(name)}
   end
 
-  def self.find_by_type(type="", series="xy")
-    file = File.open("#{DATA_DIR}/#{series}/#{LANGUAGE}/type_table.json")
+  def self.find_by_type(type="")
+    file = File.open("#{PokemonZukan.file_dir}/type_table.json")
     json = JSON.parser.new(file.read())
     file.close
 
     hash = json.parse()
-    PokemonZukan::find_all(hash[type], series)
+    PokemonZukan::find_all(hash[type])
   end
 
-  def self.find_all_by_type(types=[], series="xy")
-    types.map{|type| PokemonZukan::find_by_type(type, series)}
+  def self.find_all_by_type(types=[])
+    types.map{|type| PokemonZukan::find_by_type(type)}
   end
 
-  def next(series="xy")
-    PokemonZukan::find(@no.to_i+1, series)
+  def next
+    PokemonZukan::find(@no.to_i+1)
   end
 
-  def prev(series="xy")
-    PokemonZukan::find(@no.to_i-1, series)
+  def prev
+    PokemonZukan::find(@no.to_i-1)
   end
 end
